fb7f1823b87e3c7a379b358208886c4f432310b1
Akito Iwakura <aki@asterleen.com>
2018-12-29 19:29:42 +0300

    Remove debug code

--- engine/trackstate.php
+++ engine/trackstate.php
@@ -3,8 +3,6 @@
 function getCurrentTrackType() {
 	$track = file_get_contents('/var/run/liquidsoap/soviet.track');
 	$pathcomponents = explode('/', trim($track));
-
-	error_log(print_r($pathcomponents, true));
-
+	
 	return ($pathcomponents[7]);
 }
