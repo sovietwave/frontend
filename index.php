592cc8be386d199941b344adf322e219c405ddc1
grouzdev <dmitry.grouzdev@gmail.com>
2023-06-30 17:07:58 +0300

    cache fix

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ $smarty->setCacheDir('engine/cache');
 $smarty->setConfigDir('engine/configs');
 
 // Change me when css/js is changed
-define ('CLIENT_VERSION', 33);
+define ('CLIENT_VERSION', 50);
 
 // Set me to true when the site works in a special (event) mode
 define ('EVENT_OVERRIDE', true);
